68bc7790dbc23626414da1211834a6635e2265fc
jlstack <stackjl@appstate.edu>
2017-09-19 16:35:25 -0400

    Added Populate.sql

---
+++ Populate.sql
@@ -0,0 +1,6 @@
+insert into product (Name, Description, Image_Path, Price) 
+values ("Red Oak Entertainment Stand", "This large table measures at 2 x 4 x 2 and is made of solid Red Oad.", "/images/entertainment_stand.jpeg", "1500.00");
+
+insert into product (Name, Description, Image_Path, Price) 
+values ("Red Oak Endtable", "This endtable was made to match the Red Oak Entertainment Stand. It measures at 2 x 2 x 2 and is also made of solid Red Oad.", "/images/endtable.jpeg", "1000.00");
+
